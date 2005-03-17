148f062c865dd83123008b37a0b8ba4383155b69
Jerome Fenal <jfenal@free.fr>
2005-03-16 16:06:54 -0800

    import Acme::MetaSyntactic::RefactorCode 0.02 from CPAN
    git-cpan-module: Acme::MetaSyntactic::RefactorCode
    git-cpan-version: 0.02
    git-cpan-authorid: JFENAL

--- lib/Acme/MetaSyntactic/RefactorCode.pm
+++ lib/Acme/MetaSyntactic/RefactorCode.pm
@@ -2,40 +2,64 @@ package Acme::MetaSyntactic::RefactorCode;
 
 use warnings;
 use strict;
-use B::Deobfuscate;
+use base 'B::Deobfuscate';
 use Acme::MetaSyntactic;
+use Data::Dumper;
+use Getopt::Std;
 
 =head1 NAME
 
-Acme::MetaSyntactic::RefactorCode - The great new Acme::MetaSyntactic::RefactorCode!
+Acme::MetaSyntactic::RefactorCode - Theme and refactor your code!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
 Acme::MetaSyntactic::RefactorCode helps you give signicant variable names to your
 code.
 
-The easy way to use it is:
+The default (I<loremipsum> theme) way to use it is:
 
-   perl -MAcme::MetaSyntactic::RefactorCode yourscript.pl > yournewscript.pl
+   perl -MAcme::MetaSyntactic::RefactorCode script.pl > loremipsumed.pl
 
-=head1 EXPORT
+Or the themed version:
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+   perl -MAcme::MetaSyntactic::RefactorCode script.pl -t theme > themed.pl
+
+Take special care to choose a theme that has sufficient entries to refactor your
+code. Use this one-liner to get available themes:
+
+   perl -MAcme::MetaSyntactic -e 'print join "\n",Acme::MetaSyntactic->themes'
+
+=head1 ABSTRACT
+
+Make your code special, using the theme of your choice available from the great
+C<Acme::MetaSyntactic> by BooK.
 
 =cut
 
-BEGIN { srand time; }
-sub _LoadDecoder { open *B::Deobfuscate::DATA, "<:scalar", join "\n", metaname(101) }
-sub import { shift; require O; O->import( 'Deobfuscate', @_ ) }
+
+BEGIN { 
+    our ( $opt_t );
+    getopt('t:');
+    $opt_t = "loremipsum" if not Acme::MetaSyntactic->has_theme($opt_t);
+    my $meta = Acme::MetaSyntactic->new( $opt_t );
+    open *B::Deobfuscate::DATA, "<:scalar", \join( "\n",
+        # Circumvent broken API that force to type more than needed, not
+        # counting this very comment               +
+        #                                          |
+        # some themes may have duplicates          |
+        #    v                                     v
+        keys %{ {map { ( $_ => 1 ) } $meta->name($opt_t => 0) } } )  ;
+}
+
+sub import { require O; O->import( 'Deobfuscate', @_ ); }
 
 =head1 AUTHOR
 
@@ -43,7 +67,13 @@ Jerome Fenal, C<< <jfenal@free.fr> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to
+AMSRC will explode if using a theme that has no sufficient entries for your
+code.
+
+B::Deobfuscate seems also to have problems with big code chunks (tried it on the
+future 0.02 L<Solaris::Disk::SVM>), some methods calls not being translated.
+
+Please report any other bugs or feature requests to
 C<bug-acme-metasyntactic-refacecode@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-MetaSyntactic-RefactorCode>.
 I will be notified, and then you'll automatically be notified of progress on
@@ -51,12 +81,15 @@ your bug as I make changes.
 
 =head1 ACKNOWLEDGEMENTS
 
-Heavily (completely) based on the idea and the code of Acme::Floral by Joshua b.
+Heavily (completely) based on the idea and the code of L<Acme::Floral> by Joshua b.
 Jore.
+
+This module could not have happened without the L<Acme::MetaSyntactic> module by
+Philippe "BooK" Bruhat.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2005 Jerome Fenal, All Rights Reserved.
+Copyright 2005 Jérôme Fenal, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
