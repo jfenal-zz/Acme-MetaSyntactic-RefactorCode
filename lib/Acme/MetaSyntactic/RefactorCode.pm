5c73f5a24564bd2d7adc7cb35a9c6a4bc85bb30c
Jerome Fenal <jfenal@free.fr>
2007-02-03 08:21:46 -0800

    import Acme::MetaSyntactic::RefactorCode 0.05 from CPAN
    git-cpan-module: Acme::MetaSyntactic::RefactorCode
    git-cpan-version: 0.05
    git-cpan-authorid: JFENAL

--- lib/Acme/MetaSyntactic/RefactorCode.pm
+++ lib/Acme/MetaSyntactic/RefactorCode.pm
@@ -12,11 +12,11 @@ Acme::MetaSyntactic::RefactorCode - Theme and refactor your code!
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
--- lib/B/Deobfuscate/Dict/RefactorCode.pm
+++ lib/B/Deobfuscate/Dict/RefactorCode.pm
@@ -8,7 +8,7 @@ use vars qw($theme);
 
 use Acme::MetaSyntactic;
 
-our VERSION = '0.04';
+our VERSION = '0.05';
 
 =head1 NAME
 
